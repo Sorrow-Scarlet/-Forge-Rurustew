4ccc36f7170af36c7397a6b5fcb59c6040bfb13c
Sorrow <61646954+Sorrow-Scarlet@users.noreply.github.com>
2024-10-10 16:55:56 +0800

    一阶段功能开发完成，暂时使用农夫乐事的培根和调色变种贴图。

--- src/main/java/SorrowIDT/rurustew/item/cookedPlayerMeat.java
+++ src/main/java/SorrowIDT/rurustew/item/cookedPlayerMeat.java
@@ -1,11 +1,8 @@
 package SorrowIDT.rurustew.item;
 
+import net.minecraft.world.food.FoodProperties;
 import net.minecraft.world.food.Foods;
 
 public class cookedPlayerMeat extends Foods {
-    public cookedPlayerMeat() {
-        super();
-
-
-    }
+    public static final FoodProperties cookedplayermeat = (new FoodProperties.Builder()).nutrition(8).saturationMod(0.8F).meat().build();
 }
